f4ac56fd4ab98c24d183c15e7b64738d5229c512
matiaselm <matias@elm.fi>
2021-10-07 15:53:21 +0300

    added translation

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
+import './i18n';
+import { useTranslation } from 'react-i18next';
 
-export default function App() {
-  const [ logo, setLogo ] = useState(false)
+const App = () => {
+  const [logo, setLogo] = useState(false)
+  const { t } = useTranslation();
 
   const handleOnPress = () => {
     setLogo(!logo)
@@ -11,15 +14,15 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Image style={{ height: 300, width: '100%', backgroundColor:'#aaa' }} source={require('./assets/mersu_porvoossa.jpg')}></Image>
+      <Image style={{ height: 300, width: '100%', backgroundColor: '#aaa' }} source={require('./assets/mersu_porvoossa.jpg')}></Image>
       <TouchableOpacity onPress={handleOnPress}>
         <Text style={{ color: '#000', fontSize: 100, textAlign: 'center' }}>:D</Text>
       </TouchableOpacity>
-      { logo === true && <Text style={{ color: 'blue', fontSize: 100, textAlign: 'center' }}>Veho Drive</Text>}
+      {logo === true && <Text style={{ color: 'blue', fontSize: 100, textAlign: 'center' }}>{t('appName')}</Text>}
       <StatusBar style="auto" />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -27,3 +30,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
   },
 });
+
+export default App
